fc43711dfb1e2f7d65fa65b47de4106ec60a8101
Triveni <trivenigedela121@gmail.com>
2025-11-13 20:45:57 +0530

    To perform calculations

---
+++ calculator.js
@@ -0,0 +1,21 @@
+const display = document.querySelector("#display")
+
+function press(val) {
+    display.value += val
+}
+
+function clearDisplay() {
+    display.value=''
+}
+
+function backspace() {
+    display.value = display.value.slice(0,-1)
+}
+function calculate() {
+    try {
+        display.value = eval(display.value)
+    }
+    catch {
+        display.value = 'Error'
+    }
+}
